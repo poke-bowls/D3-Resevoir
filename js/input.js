28312de7955246cf1c076b019bab9770cb811b29
Micah Koki <micah.koki@gmail.com>
2017-03-04 21:30:05 -1000

    Add math for pipe size calcuation based on rate input.

--- js/input.js
+++ js/input.js
@@ -15,17 +15,45 @@ function findValve() {
   var arr = document.getElementsByName('rate');
 
   for(var i = 0; i < arr.length; i++) {
-    if(parseInt(arr[i].value) && document.getElementById('xRate-value').id == arr[i].id) {
+    if(parseInt(arr[i].value) && document.getElementById('xRateInput').id == arr[i].id) {
       valve = Math.ceil((parseInt(arr[i].value) * 1.5625)/5)*5;
-      document.getElementById('xValve-value').value = valve;
-
+      document.getElementById('xValveValue').value = valve;
+    } else if(parseInt(arr[i].value) && document.getElementById('yRateInput').id == arr[i].id) {
+      valve = Math.ceil((parseInt(arr[i].value) * 1.5625)/5)*5;
+      document.getElementById('yValveValue').value = valve;
     } else {
-      valve = Math.ceil((parseInt(arr[i].value) * 1.5625)/5)*5;
-      document.getElementById('yValve-value').value = valve;
+      document.getElementById('xValveValue').value = 0;
+      document.getElementById('yValveValue').value = 0;
     }
   }
 }
 
 function findPipeSize() {
   var pipeSizes = [8,10,12,16,20,25,32,40,50,63,80,100,127,160,201,254,320,404,509,642,810,1021,1288,1624,2048,2582,3256,4106,5178,6529,8233,10382,13091,16507,20815,26248,33098,41735,52627,66361,83680,105518,133056,167780,211566,250000,300000,350000,400000,450000,500000,600000,700000,750000,800000,900000,1000000,1250000,1500000,1750000,2000000];
+  var size = 0;
+  var zSize = 0;
+  var arr = document.getElementsByName('rate');
+
+  for(var i = 0; i < arr.length; i++) {
+    if(parseInt(arr[i].value) && document.getElementById('xRateInput').id == arr[i].id) {
+      size = Math.ceil((parseInt(arr[i].value) * 4468.56)/4.16);
+      for(var j = 0; j < pipeSizes.length; j++) {
+        if(size <= pipeSizes[j]) {
+          document.getElementById('xPipeValue').value = pipeSizes[j];
+          break;
+        }
+      }
+    } else if(parseInt(arr[i].value) && document.getElementById('yRateInput').id == arr[i].id) {
+      size = Math.ceil((parseInt(arr[i].value) * 4468.56)/4.16);
+      for(var k = 0; k < pipeSizes.length; k++) {
+        if(size <= pipeSizes[k]) {
+          document.getElementById('yPipeValue').value = pipeSizes[k];
+          break;
+        }
+      }
+    }
+  }
+  zSize += Math.ceil((document.getElementById('total').value * 4468.56)/4.16);
+  document.getElementById('zPipeValue').value = zSize;
+
 }
